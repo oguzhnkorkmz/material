937bf7987d4c73bea85c41c927b0dff30c5ff300
oguzhn=korkmz <oguz.korkmz.2594@icloud.com>
2023-05-29 20:36:13 +0300

    multiCheckbox

--- src/components/LessonCheckbox.tsx
+++ src/components/LessonCheckbox.tsx
@@ -12,7 +12,20 @@ import {
 export const LessonCheckbox = () => {
 
     const [isAccept, setIsAccept] = useState(false)
-    console.log(isAccept)
+
+    const [knowledge, setKnowledge] = useState<string[]>([])
+
+    const handleKnowledgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const index = knowledge.indexOf(e.target.value)
+        if (index === -1) {
+            setKnowledge([...knowledge, e.target.value])
+        } else {
+            setKnowledge(knowledge.filter(item => item !== e.target.value))
+        }
+    }
+
+    console.log(knowledge)
+
     return (
         <Box>
             <Box>
@@ -32,6 +45,25 @@ export const LessonCheckbox = () => {
                         <FormControlLabel
                             control={<Checkbox checked={isAccept} onChange={e => setIsAccept(e.target.checked)} />}
                             label='I Accept the Terms of Use' />
+                    </FormGroup>
+                </FormControl>
+            </Box>
+            <Box>
+                <FormControl>
+                    <FormLabel>What do You Know in Frontend </FormLabel>
+                    <FormGroup row>
+                        <FormControlLabel
+                            value='react'
+                            control={<Checkbox checked={knowledge.includes('react')} onChange={handleKnowledgeChange} />}
+                            label='React' />
+                        <FormControlLabel
+                            value='angular'
+                            control={<Checkbox checked={knowledge.includes('angular')} onChange={handleKnowledgeChange} />}
+                            label='Angular' />
+                        <FormControlLabel
+                            value='vue'
+                            control={<Checkbox checked={knowledge.includes('vue')} onChange={handleKnowledgeChange} />}
+                            label='Vue' />
                     </FormGroup>
                 </FormControl>
             </Box>
